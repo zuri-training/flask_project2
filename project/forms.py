1f3c2e4ba3cc2db94c60fe7300655bddc7df0757
Sikiru Yusuff <sikiru.yusuff@yahoo.com>
2023-03-09 17:19:29 +0100

    first commit

---
+++ project/forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import StringField, IntegerField, SubmitField
+from flask_wtform import Form
+
+class NameForm(self):
+    name = StringField()
---
+++ project/routes.py
@@ -0,0 +1,34 @@
+from project import app, db
+from flask import render_template, url_for, redirect, flash, request, session
+from project.models import User, Role
+from project.forms import NameForm
+from flask_mail import Message, Mail
+
+app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+
+        else:
+            session['known'] = True
+
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
